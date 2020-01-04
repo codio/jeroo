6fcd2b0eebd73f16df684e322969d5266cc08f2d
Ben Konz <benkonz16@gmail.com>
2020-01-04 15:21:48 -0500

    minor refactoring

--- src/app/dashboard/file-save.service.ts
+++ src/app/dashboard/file-save.service.ts
@@ -7,20 +7,14 @@ export class FileSaveService {
     constructor() { }
 
     saveBlob(fileSaver: HTMLAnchorElement, blob: Blob, fileName: string) {
-        const saveBlob = (function () {
-            return function () {
-                if (window.navigator && window.navigator.msSaveOrOpenBlob) {
-                    window.navigator.msSaveOrOpenBlob(blob, fileName);
-                } else {
-                    const url = window.URL.createObjectURL(blob);
-                    fileSaver.href = url;
-                    fileSaver.download = fileName;
-                    fileSaver.click();
-                    window.URL.revokeObjectURL(url);
-                }
-            };
-        }());
-
-        saveBlob();
+        if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+            window.navigator.msSaveOrOpenBlob(blob, fileName);
+        } else {
+            const url = window.URL.createObjectURL(blob);
+            fileSaver.href = url;
+            fileSaver.download = fileName;
+            fileSaver.click();
+            window.URL.revokeObjectURL(url);
+        }
     }
 }
